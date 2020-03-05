3148d4c0782ebc71dde8a2949f331e56ebdd3302
jehannes <37177127+jehannes@users.noreply.github.com>
2020-03-05 15:03:39 +0100

    Add files via upload
    small update

--- multisorter_v3.ps1
+++ multisorter_v3.ps1
@@ -1,15 +1,14 @@
-#maybe check if file exists in target folder if so delete else move over
-#maybe add check to files if they are more than a day old
-
  #folders to sort
  $Folders = "Downloads", "Documents"
 
- $targets=#an array with tuples: target folder & an array of extension to be sorted to it (sub targets)
+ $targets=#an array with tuples: target folder & an array of extensions to be sorted to it (sub targets)
  @("PDF",@("pdf")),
+ @("TORRENTS",@("torrent")),
+ @("PACKETTRACER",@("pkt")),
  @("POWERPOINT",@("ppt*")),
  @("EXE",@("exe","msi")),
  @("EXCEL",@("xlr","cls","xls*")),
- @("WPF",@("odt","tex","wps","wks","txt","txt","doc","rtf","wpd","docx")),
+ @("WPF",@("odt","tex","wps","wks","txt","doc*","rtf","wpd")),
  @("AUDIO",@("aif","cda","mid","mp3","mpa","ogg","wav","wma","wpl","midi")),
  @("IMAGES",@("ai","ps","bmp","gif","ico","jpg","png","psd","svg","tif","pbm","pgm","ppm","tiff","jpeg")),
  @("VIDEOS",@("rm","swf","vob","wmv","3gp","avi","flv","m4v","mkv","mov","mp4","3g2","mpeg","h264","h265","hvec")),
@@ -17,7 +16,7 @@
  @("CODE",@("c","h","md","ui","py","cs","sh","pl","vb","asp","pyc","pro","cpp","hpp","css","xml","pdb","dll","sql","cer","cgi","htm","jsp","rss","cfm","part","java","aspx",
  "xaml","html","json","make","class","xhtml","swift","stash","sqlite","release",".pro.user","gitignore"))
  
-
+ $DateLimit= (Get-Date).AddDays(-1)#limits date to more than a day olds
 
 
  for($i=0; $i -lt $Folders.count; $i++){
@@ -40,13 +39,28 @@
 
         #sort all sub targets for a given target folder
         forEach($element in $targets[$j][1]){
-                Get-ChildItem -Path $basePath$element | Select-Object FullName | ForEach-Object {Move-Item -Path $_.FullName -Destination $curPath}
+            Get-ChildItem -Path $basePath$element |Where-Object {$_.CreationTime -lt $DateLimit} | ForEach-Object {      
+                $targetPath= $curPath+"\"+$_.Name
+                if(-not (Test-Path $targetPath)){#check if the same file exists in the destination folder
+                    Select-Object FullName
+                    Move-Item -Path $_.FullName -Destination $curPath
+                }
+                else{#if it is 
+                    Select-Object FullName
+                    Remove-Item -Path $_.FullName
+                }
+            }
         }
     }
 
     #special cases
+    Move-Item -Path $env:USERPROFILE\$curFolder\*README* -Destination $env:USERPROFILE\$curFolder\UNSORTED\CODE
     Move-Item -Path $env:USERPROFILE\$curFolder\*LICENSE* -Destination $env:USERPROFILE\$curFolder\UNSORTED\CODE
+    Move-Item -Path $env:USERPROFILE\$curFolder\*makefile* -Destination $env:USERPROFILE\$curFolder\UNSORTED\CODE
+    Move-Item -Path $env:USERPROFILE\$curFolder\*Makefile* -Destination $env:USERPROFILE\$curFolder\UNSORTED\CODE
+    Move-Item -Path $env:USERPROFILE\$curFolder\*MakeFile* -Destination $env:USERPROFILE\$curFolder\UNSORTED\CODE
+    Move-Item -Path $env:USERPROFILE\$curFolder\*MAKEFILE* -Destination $env:USERPROFILE\$curFolder\UNSORTED\CODE
 
     #remaining/other files
-    Get-ChildItem -Path $env:USERPROFILE\$curFolder\* | where { !$_.PSisContainer } | Select-Object FullName| ForEach-Object {Move-Item -Path $_.FullName -Destination $env:USERPROFILE\$curFolder\UNSORTED\OTHER }
+    Get-ChildItem -Path $env:USERPROFILE\$curFolder\* | where { !$_.PSisContainer -and $_.CreationTime -lt $DateLimit} | Select-Object FullName| ForEach-Object {Move-Item -Path $_.FullName -Destination $env:USERPROFILE\$curFolder\UNSORTED\OTHER }
 }
